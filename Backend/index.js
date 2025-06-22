f2ebde2618a85ed7a69c7684689db8a7534be74e
Mr KUNAL NAURIYAL <89171857+kunal-Nauriyal@users.noreply.github.com>
2025-06-22 19:25:03 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ connectDB();
 
 // CORS Middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000' , 'https://gigbud-testing-111.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
